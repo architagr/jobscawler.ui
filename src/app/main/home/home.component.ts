c1d15f301bbeaaeb4fd9db2c277bf18ae0582050
Gaurav Agarwal <grwlgrv123@gmail.com>
2023-03-25 06:38:06 -0700

    UI changes

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginPopupComponent } from '../login-popup/login-popup.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(public dialog: MatDialog){}
+
+  openDialog() {
+    this.dialog.open(LoginPopupComponent);
+  }
 
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoginPopupComponent } from './login-popup/login-popup.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    LoginPopupComponent
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatDialogModule
+  ],
+  entryComponents:[
+    LoginPopupComponent 
   ]
 })
 export class MainModule { }
