31bbcbe89581545f7e28a6b51feb9ba736ac16e8
Gaurav Agarwal <grwlgrv123@gmail.com>
2023-04-07 23:25:57 +0530

    candidate module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CandidateModule } from './candidate/candidate.module';
+import { MainModule } from './main/main.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>MainModule
+  },
+  {
+    path:'candidate',
+    loadChildren:()=>CandidateModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
